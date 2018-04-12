e09c5273559749e3ccafc69e0ab1788264a8dea1
Jordane21 <38005486+Jordane21@users.noreply.github.com>
2018-04-13 01:42:54 +0200

    Update trader_de_l_obscur.rb

--- trader_de_l_obscur.rb
+++ trader_de_l_obscur.rb
@@ -1,1 +1,12 @@
+
+require 'rubygems'
+require 'nokogiri'   
+require 'open-uri'
+
+page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))  
+
+
+
+
+
 #récupérer le cours de toutes les cryptomonnaies, et les enregistrer bien proprement dans une array de hashs
